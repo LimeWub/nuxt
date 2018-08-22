14b4485a1ee006c6c2887da73f48725aff7490db
LimeWub <myrto.kontouli@gmail.com>
2018-08-22 16:13:14 +0100

    Playing around progress

---
+++ components/Card.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  article.card
+      div.card__container
+        div.card__content
+            h1 123
+            p 456
+</template>
+
+
+<script>
+export default {
+  data: () => {
+    return {
+      bob: 'Because something about visions here'
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+  .card {
+    width: 100%;
+    background: grey;
+    position: relative;
+
+    &::after {
+        content: '';
+        display: block;
+        padding-top: 50%;
+    }
+  }
+
+  .card__container {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+
+  .card__content {
+      color: white;
+  }
+</style>
---
+++ pages/portfolio.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  section.container
+      div
+        h1 Hello darkness my old friend
+        h2 Heading 2
+        h3 Heading 3
+        h4 Heading 4
+        h5 Heading 5
+        h6 Heading 6
+
+        p I've come to talk to you MULTI
+        | LINE 
+        | {{ bob }}
+        |<br>
+        | I've come to talk to you MULTI
+        | LINE 
+        | {{ bob }}
+        |<br>
+        | I've come to talk to you MULTI
+        | LINE 
+        | {{ bob }}
+        |<br>
+        | I've come to talk to you MULTI
+        | LINE 
+        | {{ bob }}
+
+        card
+
+        div.card
+          h1 Who dis
+</template>
+
+
+<script>
+import Card from "~/components/Card";
+
+export default {
+  data: () => {
+    return {
+      bob: "Because something about visions here"
+    };
+  },
+  components: {
+    Card
+  }
+};
+</script>
+
+<style lang="scss">
+.container {
+  text-align: center;
+  color: red;
+}
+</style>
